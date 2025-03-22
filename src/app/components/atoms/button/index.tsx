41ef9286e5391295e7d5cd4400d48b754c49f924
David Montaño <155041313+XavierM25@users.noreply.github.com>
2025-03-22 17:45:45 -0500

    feat(button): add icon support and customizable font styles

--- src/app/components/atoms/button/index.tsx
+++ src/app/components/atoms/button/index.tsx
@@ -1,6 +1,10 @@
-"use client"; // Necesario porque usamos interactividad (onClick)
+"use client";
 
 import React from "react";
+import { ArrowLeft, ArrowRight } from "@mynaui/icons-react";
+
+type FontFamily = "Poppins" | "Onest";
+type FontWeight = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900;
 
 interface ButtonProps {
   children: React.ReactNode;
@@ -9,9 +13,14 @@ interface ButtonProps {
   className?: string;
   textColor?: string;
   bgColor?: string;
+  hoverColor?: string;
   disabled?: boolean;
-  icon?: React.ReactNode; // Ahora acepta un React.ReactNode (como un componente de React Icons)
+  icon?: "ArrowLeft" | "ArrowRight";
   iconPosition?: "left" | "right";
+  iconSize?: number;
+  iconColor?: string;
+  fontFamily?: FontFamily;
+  fontWeight?: FontWeight;
 }
 
 const Button: React.FC<ButtonProps> = ({
@@ -21,20 +30,47 @@ const Button: React.FC<ButtonProps> = ({
   className,
   textColor = "text-white",
   bgColor = "bg-blue-500",
+  hoverColor = "hover:bg-blue-600",
   disabled = false,
   icon,
   iconPosition = "left",
+  iconSize = 20,
+  iconColor = "currentColor",
+  fontFamily = "Poppins",
+  fontWeight = 400,
 }) => {
+  const renderIcon = () => {
+    if (!icon) return null;
+
+    switch (icon) {
+      case "ArrowLeft":
+        return <ArrowLeft size={iconSize} color={iconColor} />;
+      case "ArrowRight":
+        return <ArrowRight size={iconSize} color={iconColor} />;
+      default:
+        return null;
+    }
+  };
+
+  const fontStyle = {
+    fontFamily,
+    fontWeight,
+  };
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`${bgColor} ${textColor} px-4 py-2 rounded flex items-center gap-2 ${className}`}
+      className={`${bgColor} ${textColor} ${hoverColor} px-4 py-2 rounded flex items-center gap-2 cursor-pointer transition-colors duration-300 ${className}`}
+      style={fontStyle}
       disabled={disabled}
     >
-      {icon && iconPosition === "left" && <span>{icon}</span>}
+      {icon && iconPosition === "left" && (
+        <span className="flex items-center">{renderIcon()}</span>
+      )}
       <span>{children}</span>
-      {icon && iconPosition === "right" && <span>{icon}</span>}
+      {icon && iconPosition === "right" && (
+        <span className="flex items-center">{renderIcon()}</span>
+      )}
     </button>
   );
 };
