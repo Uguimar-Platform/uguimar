b229a7d1b5bf43429ee2531239140ddb38366f34
KenithGP <kenithguanilo@gmail.com>
2025-03-24 02:04:25 -0500

    feat: add Hero component to Landing page

--- src/app/pages/Landing.tsx
+++ src/app/pages/Landing.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import LearningPathSection from "../components/organisms/learnigPath";
 import ContactSection from "../components/organisms/contact";
+import Hero from "../components/organisms/hero";
 
 
 
 const Landing = () => {
   return (
     <>
+      <Hero />
       <LearningPathSection />
       <ContactSection />
     </> 
