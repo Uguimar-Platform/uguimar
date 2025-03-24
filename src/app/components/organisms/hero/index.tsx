3213c29c26669b1389b2be09f28def4f4a369674
David Montaño <155041313+XavierM25@users.noreply.github.com>
2025-03-24 01:10:20 -0500

    refactor(hero): update hero section layout and styling

--- src/app/components/organisms/hero/index.tsx
+++ src/app/components/organisms/hero/index.tsx
@@ -2,76 +2,74 @@
 import React from "react";
 import { Label } from "../../atoms/label";
 import Button from "../../atoms/button";
-import InputAtom from "../../atoms/input";
-import { Search } from "@mynaui/icons-react";
+
 const Hero: React.FC = () => {
   return (
-    <section className="bg-blue-100 p-10 rounded-lg flex flex-col gap-6 items-center text-center">
-      <div className="flex flex-row items-center gap-6 max-w-[1000px] mx-auto">
-        <div>
+    <section className="p-10 rounded-lg flex flex-col gap-6 items-center text-center">
+      <div className="flex flex-row items-center gap-6">
+        <div className="max-w-[610px] -mt-22">
           <div className="max-w-2xl">
             <Label
               as="h1"
               text="Aprende, crece y"
               textColor="text-black"
-              font="Poppins"
+              font="sfpro"
               fontWeight={700}
-              className="text-4xl"
+              className="text-6xl"
             />
+            <div className="flex-grow flex space-x-3">
+              <Label
+                as="h1"
+                text="destaca"
+                textColor="text-purple"
+                font="sfpro"
+                fontWeight={700}
+                className="text-6xl text-blue-900"
+              />{" "}
+              <Label
+                as="h1"
+                text="con cursos"
+                textColor="text-black"
+                font="sfpro"
+                fontWeight={700}
+                className="text-6xl"
+              />
+            </div>
             <Label
               as="h1"
-              text="destaca"
-              textColor="text-purple"
-              font="Poppins"
-              fontWeight={700}
-              className="text-4xl text-blue-900"
-            />{" "}
-            <Label
-              as="h1"
-              text="con cursos transformadores"
+              text=" transformadores"
               textColor="text-black"
-              font="Poppins"
+              font="sfpro"
               fontWeight={700}
-              className="text-4xl"
+              className="text-6xl"
             />
             <Label
               as="p"
               text="Tu plataforma de aprendizaje: crece, explora y alcanza tus metas con cursos que transforman."
               textColor="text-gray-600"
-              font="Poppins"
+              font="sfpro"
               fontWeight={400}
-              className="mt-4 text-lg text-start"
-            />
-          </div>
-
-          <div className="bg-[#d0e3ff] rounded-lg">
-            <InputAtom
-              type="text"
-              name="search"
-              placeholder="¿Qué deseas aprender?"
-              className="bg-[#e7f1ff]"
+              className="mt-6 text-2xl text-start mb-12"
             />
           </div>
 
           <Button
-            bgColor="bg-blue-500"
+            bgColor="bg-[#7096D1]"
             hoverColor="hover:bg-blue-600"
             textColor="text-white"
-            fontFamily="Poppins"
-            fontWeight={500}
+            fontFamily="Onest"
+            fontWeight={600}
             icon="ArrowRight"
             iconPosition="right"
-            className="rounded-full"
+            className="rounded-full px-8 py-3 text-lg"
           >
             Empezar gratis
           </Button>
         </div>
-        <div>
-          <img
-            src="https://res.cloudinary.com/uguimar/image/upload/v1631590646/uguimar/hero-image_1_xjzv8o.png"
-            alt="Hero"
-            className="w-96"
-          />
+        <div className="">
+          <img src="/img_hero1.webp" alt="Hero" className="w-[400px] h-full" />
+          <img src="/img_hero2.webp" alt="Hero" className="w-[200px] h-full" />
+          <img src="/img_hero3.webp" alt="Hero" className="w-[200px] h-full" />
         </div>
       </div>
     </section>
