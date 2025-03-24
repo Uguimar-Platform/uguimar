e292163657158d2e21be29b22251c88b22551400
David Montaño <155041313+XavierM25@users.noreply.github.com>
2025-03-23 20:34:05 -0500

    refactor: remove unused components and clean up Home page

--- src/app/components/molecules/CardCategoryCourses/index.tsx
+++ src/app/components/molecules/CardCategoryCourses/index.tsx
@@ -19,14 +19,12 @@ const CardCategoryCourses: React.FC<CardCategoryCoursesProps> = ({
 }) => {
   return (
     <div className="w-[380px] bg-[#F9FCFF] border border-[#334EAC] rounded-3xl p-6 py-8 shadow-md">
-      {/* Titulo a complementacion*/}
       <Label
         text={title}
         fontWeight={700}
         font="sfpro"
         className=" text-3xl text-left "
       />
-      {/* Cursos*/}
       <Label
         text={course}
         textColor="text-[#334EAC]"
@@ -34,15 +32,12 @@ const CardCategoryCourses: React.FC<CardCategoryCoursesProps> = ({
         fontWeight={700}
         font="sfpro"
       />
-      {/* Descripción */}
       <Label text={description} className="text-gray-700 mt-2 flex" />
-      {/* Imagen */}
       <img
         className="w-30 mx-auto m-5 justify-center"
         src={categorycourseimage}
         alt={`${course}.webp`}
       />
-      {/* Botón */}
       <div className="flex justify-center">
         <Button className="bg-[#334EAC] text-white px-4 py-2 rounded-lg">
           {buttonText}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,4 @@
 "use client";
-
-import CardCategoryCourses from "./components/molecules/CardCategoryCourses";
-import LearningPathSection from "./components/molecules/learnigPath";
-
 export default function Home() {
-  return <>
-  <CardCategoryCourses 
-      title="Curso de programación"
-      course="javascript"
-      description="Aprende a programar desde cero"
-      categorycourseimage="/categorycourse.webp"
-      buttonText="Inscríbete ahora"
-  />
-  <LearningPathSection />
-  </>;
+  return <></>;
 }
