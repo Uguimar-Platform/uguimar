c8eb0435c05e541d13d7a04eb4b28a569c6a7332
David Montaño <155041313+XavierM25@users.noreply.github.com>
2025-03-26 02:26:42 -0500

    feat(courses): implement interactive course exploration section

--- src/app/components/molecules/CardCategoryCourses/index.tsx
+++ src/app/components/molecules/CardCategoryCourses/index.tsx
@@ -2,42 +2,12 @@ import React from "react";
 import Button from "../../atoms/button";
 import LabelAtom from "../../atoms/label";
 
-type FontFamily = "Poppins" | "Onest" | "SFProDisplay";
-type FontWeight =
-  | 100
-  | 200
-  | 300
-  | 400
-  | 500
-  | 600
-  | 700
-  | 800
-  | 900
-  | "thin"
-  | "extralight"
-  | "light"
-  | "normal"
-  | "regular"
-  | "medium"
-  | "semibold"
-  | "bold"
-  | "extrabold"
-  | "black";
-
 interface CardCategoryCoursesProps {
   title?: string;
   course?: string;
   description?: string;
   categorycourseimage?: string;
   buttonText?: string;
-  titleFontFamily?: FontFamily;
-  titleFontWeight?: FontWeight;
-  courseFontFamily?: FontFamily;
-  courseFontWeight?: FontWeight;
-  descriptionFontFamily?: FontFamily;
-  descriptionFontWeight?: FontWeight;
-  buttonFontFamily?: FontFamily;
-  buttonFontWeight?: FontWeight;
 }
 
 const CardCategoryCourses: React.FC<CardCategoryCoursesProps> = ({
@@ -46,46 +16,43 @@ const CardCategoryCourses: React.FC<CardCategoryCoursesProps> = ({
   description = "",
   categorycourseimage = "",
   buttonText = "",
-  titleFontFamily = "SFProDisplay",
-  titleFontWeight = 700,
-  courseFontFamily = "SFProDisplay",
-  courseFontWeight = 700,
-  descriptionFontFamily = "Poppins",
-  descriptionFontWeight = "regular",
-  buttonFontFamily = "Poppins",
-  buttonFontWeight = "medium",
 }) => {
   return (
     <div className="w-[380px] bg-[#F9FCFF] border border-[#334EAC] rounded-3xl p-6 py-8 shadow-md">
       <LabelAtom
         text={title}
-        fontWeight={titleFontWeight}
-        fontFamily={titleFontFamily}
+        fontWeight="bold"
+        fontFamily="SFProDisplay"
+        fontSize={32}
         className="text-3xl text-left"
       />
       <LabelAtom
         text={course}
-        textColor="text-[#334EAC]"
-        className="text-3xl"
-        fontWeight={courseFontWeight}
-        fontFamily={courseFontFamily}
+        textColor="#334EAC"
+        className="text-3xl mb-2"
+        fontWeight="bold"
+        fontFamily="SFProDisplay"
+        fontSize={32}
       />
       <LabelAtom
         text={description}
-        className="text-gray-700 mt-2 flex"
-        fontFamily={descriptionFontFamily}
-        fontWeight={descriptionFontWeight}
+        className="flex leading-tight"
+        fontFamily="SFProDisplay"
+        fontWeight="regular"
+        textColor="#202020"
+        fontSize={18}
       />
       <img
-        className="w-30 mx-auto m-5 justify-center"
+        className="w-30 mx-auto m-6 justify-center"
         src={categorycourseimage}
         alt={`${course}.webp`}
       />
       <div className="flex justify-center">
         <Button
-          className="bg-[#334EAC] text-white px-4 py-2 rounded-lg"
-          fontFamily={buttonFontFamily}
-          fontWeight={buttonFontWeight}
+          className="bg-[#334EAC] text-white px-16 py-2 rounded-full mt-4 "
+          fontFamily="Onest"
+          fontWeight={800}
+          hoverColor="#263a7e"
         >
           {buttonText}
         </Button>
