f098561b8254dfa02c81169bad3c0dae6c2ae163
Jampier Caramantin <jampiercaramantin06@gmail.com>
2025-03-26 01:26:44 -0500

    titulo y desc reutilizable y se quito la logica

--- src/app/components/molecules/formConfirmMail/index.tsx
+++ src/app/components/molecules/formConfirmMail/index.tsx
@@ -3,41 +3,28 @@ import React, { useState } from "react";
 import ButtonAtom from "../../atoms/button";
 import InputAtom from "../../atoms/input";
 import LabelAtom from "../../atoms/label";
-import { Divide } from "lucide-react";
 import { title } from "process";
 
-const FormConfirmMail = () => {
-  // State to store the security code (an array of six empty strings)
-  const [code, setCode] = useState<string[]>(new Array(6).fill(""));
-  // State to track if there is an error (e.g., an incomplete code)
-  const [error, setError] = useState<boolean>(false);
+export interface FormConfirmProps {
+  title: string;
+  textDescription: string;
+  
+}
 
-  /**
-   * Handles input change in the security code fields.
-   * @param index - The position of the digit being entered.
-   * @param value - The new value entered by the user.
-   */
-  const handleChange = (index: number, value: string) => {
-    if (/^\d?$/.test(value)) {
-      const newCode = [...code];
-      newCode[index] = value;
-      setCode(newCode);
-      setError(false); // Reset error when a valid input is entered
-      if (value && index < 5) {
-        document.getElementById(`code-input-${index + 1}`)?.focus();
-      }
-    }
-  };
+const FormConfirmMail: React.FC<FormConfirmProps> = ({
+  title = "Confirma tu cuenta",
+  textDescription = "En su correo llegó un código para confirmar la cuenta",
+
+}) => {
+
+  // State to store the security code (an array of six empty strings)
+  const [code] = useState<string[]>(new Array(6).fill(""));
+
+
   /**
    * Handles form submission.
    * @param e - The form submit event.
    */
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (code.some((num) => num === "")) {
-      setError(true);
-    }
-  };
 
   const inputsCode = code.map((num, index) => (
     <InputAtom
@@ -45,14 +32,12 @@ const FormConfirmMail = () => {
       name={`code-input-${index}`}
       type="text"
       value={num}
-      onChange={(e) => handleChange(index, e.target.value)}
-      className={`w-14 h-16 text-center text-xl font-semibold border-1 rounded-md bg-blue-100 focus:outline-none focus:border-blue-500 ${
-        error && num === "" ? "border-red-500" : "border-[#334EAC]"
-      }`}
+  
+      className={`w-14 h-16 text-center text-xl font-semibold border-1 rounded-md bg-blue-100 focus:outline-none focus:border-blue-500"`}
     />
   ));
 
-  const title = (
+  const titleLabelAtom = (
     <LabelAtom
       className="py-2 "
       as="h2"
@@ -60,11 +45,11 @@ const FormConfirmMail = () => {
       fontSize={40}
       fontWeight={800}
       textColor="#334EAC"
-      text="Confirma tu cuenta"
+      text={title}
     />
-  )
+  );
 
-  const parafo = (
+  const textDescriptionLabelAtom = (
     <LabelAtom
       className=" pb-10"
       as="p"
@@ -72,14 +57,14 @@ const FormConfirmMail = () => {
       fontSize={18}
       fontWeight={300}
       textColor="#081f5c"
-      text="En su correo llegó un código para confirmar la cuenta"
+      text={textDescription}
     />
-  )
+  );
 
   const subtitle = (
     <LabelAtom
       className="mb-4"
-      as = "h3"
+      as="h3"
       fontFamily="Poppins"
       fontSize={20}
       fontWeight={600}
@@ -90,13 +75,13 @@ const FormConfirmMail = () => {
 
   return (
     <div className="flex flex-col items-center justify-center">
+      {titleLabelAtom}
 
-      {title}
-      {parafo}
+      {textDescriptionLabelAtom}
       <div className="flex flex-col items-center py-6 px-8  border border-[#334EAC] rounded-[25px] max-w-sm mx-auto bg-white box-content">
         {subtitle}
         <form
-          onSubmit={handleSubmit}
+          
           className="w-full flex flex-col items-center"
         >
           <div className="flex gap-1 mb-5">{inputsCode}</div>
