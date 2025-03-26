6f108cd4ebc8fd1aaf4923e760dc923293e3aeaf
Marisola20 <emedinada@ucvvirtual.edu.pe>
2025-03-26 18:09:59 -0500

    refactor(formRenewPass): improve form structure and label usage

--- src/app/components/molecules/formRenewPass/index.tsx
+++ src/app/components/molecules/formRenewPass/index.tsx
@@ -1,21 +1,25 @@
 
 import React from "react";
 import LabelAtom from "../../atoms/label";
-import InputAtom from "../../atoms/input";
 import ButtonAtom from "../../atoms/button";
-import { Formik, Field, Form } from "formik";
-
+import InputField from "../inputField";
+import { Formik, Form } from "formik";
 
 const PasswordReset = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-blue-50">
-            <div className="p-8 rounded-2xl w-full max-w-lg">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+            <div className="p-8 rounded-2xl w-full max-w-2xl">
                 <div className="flex flex-col items-center mb-6">
-                    <img src="/path-to-your-image.jpg" alt="Password Icon" className="w-16 h-16 mb-4" />
-                    <h2 className="text-3xl font-semibold text-[#334EAC]">Nuevas credenciales</h2>
-                    <p className="text-[#081F5C]">Tu identidad fue verificada!, ingresa tu nueva contraseña</p>
+                    <img src="/AssetKey.svg" alt="Password Icon" className="w-16 h-16 mb-4" />
+                    <LabelAtom fontFamily="Onest" fontSize={40} fontWeight={800} textColor="#334EAC" 
+                    text="Nuevas credenciales" 
+                    />
+
+                    <LabelAtom fontFamily="Onest" fontSize={18} fontWeight={300} textColor="#081F5C"
+                    text="Tu identidad fue verificada!, ingresa tu nueva contraseña" 
+                    />
                 </div>
-                <Formik
+                <Formik 
                     initialValues={{ password: '', confirmPassword: '' }}
                     onSubmit={(values) => {
                         console.log('Form values:', values);
@@ -23,35 +27,41 @@ const PasswordReset = () => {
                 >
                     {() => (
                         <Form className="space-y-4">
-                            <div className="pr-8 pl-8 pt-6 pb-6 rounded-2xl border-1 border-[#334EAC] max-w-md">
-                                <div className="flex items-center gap-2 mb-2">
-                                    <img src="../public/mail-icon.svg" alt="Icon" className="w-6 h-6" />
-                                    <LabelAtom text="Nueva contraseña" className="!text-[#334EAC]" />
-                                </div>
+                            <div className="py-6 px-8 rounded-2xl border border-[#334EAC] bg-white max-w-md w-full mx-auto">
                                 <div className="relative">
-                                    <Field
+                                    <InputField
+                                        icon="Envelope"
+                                        iconPosition="left"
+                                        label="Nueva contraseña"
+                                        labelClassName="!text-[#334EAC]"
+                                        labelFontFamily="Poppins"
+                                        labelFontWeight={400}
+                                        inputFontFamily="Poppins"
+                                        inputClassName="w-full !text-[#334EAC]"
                                         name="password"
-                                        type="password"
-                                        as={InputAtom}
-                                        className="w-full p-3 mb-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                        type="password"                
+                                        className="w-full mb-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                                     />
                                 </div>
-
-                                <div className="flex items-center gap-2 mb-2">
-                                    <img src="../public/mail-icon.svg" alt="Icon" className="w-6 h-6" />
-                                    <LabelAtom text="Confirmar contraseña" className="!text-[#334EAC]" />
-                                </div>
                                 <div className="relative">
-                                    <Field
+                                    <InputField
+                                        icon="Envelope"
+                                        label="Confirmar contraseña"
+                                        labelClassName="!text-[#334EAC]"
+                                        labelFontFamily="Poppins"
+                                        labelFontWeight={400}
+                                        inputFontFamily="Poppins"
+                                        inputClassName="w-full !text-[#334EAC]"
                                         name="confirmPassword"
                                         type="password"
-                                        as={InputAtom}
-                                        className="w-full p-3 mb-4 rounded-lg  focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                        className="w-full mb-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                                     />
                                 </div>
                                 <ButtonAtom
                                     type="submit"
-                                    className="w-full p-3 justify-center bg-[#334EAC] text-white rounded-lg hover:bg-blue-700"
+                                    bgColor="#334EAC"
+                                    textColor="#fff"
+                                    className="w-full p-3 justify-center rounded-lg hover:bg-blue-700"
                                 >
                                     Actualizar
                                 </ButtonAtom>
