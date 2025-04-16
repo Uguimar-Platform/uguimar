d931d91443ea5bb2ce92037c2304d7680581fcf8
Eduardo Saldaña <lsaldanatu@ucvvirtual.edu.pe>
2025-04-16 09:53:37 -0500

    feat(responsive): added responsive layout to LoginOrganism component

--- src/app/components/organisms/login/index.tsx
+++ src/app/components/organisms/login/index.tsx
@@ -7,12 +7,12 @@ import FormLogin from "../../molecules/formLogin";
 
 export default function LoginOrganism() {
     return (
-        <section className="flex flex-col md:flex-row items-center justify-center gap-8 p-12 bg-[#F5FAFF]">
-            <div className="flex flex-row gap-4 p-12 rounded-2xl border border-[#D0D5DD] bg-white">
-                <div>
+        <section className="flex flex-col items-center justify-center p-6 md:p-12">
+            <div className="flex flex-col md:flex-row gap-6 md:gap-12 p-6 md:p-12 rounded-2xl border border-[#D0D5DD] bg-white w-full max-w-5xl">
+                <div className="w-full md:w-1/2">
                     <FormLogin />
                 </div>
-                <div className="flex flex-col items-center text-center max-w-md">
+                <div className="w-full md:w-1/2 flex flex-col items-center text-center">
                     <DynamicIcon
                         labelText="Aprende, crece y destaca con cursos prácticos y accesibles en un solo lugar."
                         labelColor="text-[#081F5C]"
