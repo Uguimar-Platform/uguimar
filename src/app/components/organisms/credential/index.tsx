da74af87c05b68d5c5248d282a80d6b7e489ab21
Anderson Villaorduña <vvillaordunasil@ucvvirtual.edu.pe>
2025-04-16 23:52:17 -0500

    feat:add-organism-credential

---
+++ src/app/components/organisms/credential/index.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import FormRenewPass from "../../molecules/formRenewPass";
+
+const Credential: React.FC = () => {
+  return (
+    <section className="flex flex-col items-center justify-center min-h-screen ">
+      <FormRenewPass />
+    </section>
+  );
+};
+
+export default Credential;
