fdc4ddae586ae541948278a55fd1fb2c6ae8717c
Marisola20 <emedinada@ucvvirtual.edu.pe>
2025-04-16 22:19:00 -0500

    refactor(organismResetPassword): "improve code structure"

--- src/app/components/organisms/organismResetPassword/index.tsx
+++ src/app/components/organisms/organismResetPassword/index.tsx
@@ -5,34 +5,31 @@ import { useRouter } from 'next/navigation';
 import ButtonAtom from '@/app/components/atoms/button';
 import FormResetPassword from '@/app/components/molecules/formResetPassword';
 
-const OrganismResetPassword: React.FC = () => {
+const organismResetPassword: React.FC = () => {
   const router = useRouter();
 
   return (
-    <section className="min-h-screen flex items-center justify-center bg-[#E7F1FF] px-4 py-10 relative">
+    <section className="min-h-screen m-20 items-center justify-center bg-white  px-6 py-10 rounded-[30px]">
+        <div className="pb-6">
+          <ButtonAtom
+            icon="ArrowLeft"
+            iconPosition="left"
+            textColor="#334EAC"
+            bgColor="transparent"
+            hoverColor="#cdddf5"
+            className="top-6 left-6 border border-[#334EAC]"
+            fontFamily="Poppins"
+            fontWeight={500}
+          >
+            Volver
+          </ButtonAtom>  
+        </div>
+        <div>
+          <FormResetPassword />
+        </div> 
       
-      <div className="absolute top-6 left-6">
-        <ButtonAtom
-          onClick={() => router.back()}
-          icon="ArrowLeft"
-          iconPosition="left"
-          textColor="#334EAC"
-          bgColor="transparent"
-          hoverColor="#cdddf5"
-          className="border border-[#334EAC]"
-          fontFamily="Poppins"
-          fontWeight={500}
-        >
-          Volver
-        </ButtonAtom>
-      </div>
-
-      
-      <div className="w-full max-w-3xl bg-white rounded-[30px] px-6 py-10 shadow-md">
-        <FormResetPassword />
-      </div>
     </section>
   );
 };
 
-export default OrganismResetPassword;
+export default organismResetPassword;
