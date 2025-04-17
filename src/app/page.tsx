b7e83dfe4d937a5227fd458019deeda5331bc468
Tony <tonysalinasdev@gmail.com>
2025-04-17 18:22:54 -0500

    chore: remove test component from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import OForgotPass from "./components/organisms/forgotPassword";
 import Landing from "./pages/Landing";
 
 export default function Home() {
   return <>
   <Landing/>
-  <OForgotPass/>
   </>;
 }
