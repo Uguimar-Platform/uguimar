6f20e967dd75b14af44217c3b954476491e9f45d
Jampier Caramantin <jampiercaramantin06@gmail.com>
2025-04-19 21:30:56 -0500

    fix: removed hook, corrected gap class and button border and Adjusted button border styling

--- src/app/components/organisms/ResetPassword/index.tsx
+++ src/app/components/organisms/ResetPassword/index.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
 import ButtonAtom from '@/app/components/atoms/button';
 import FormResetPassword from '@/app/components/molecules/formResetPassword';
 
 const ResetPassword: React.FC = () => {
-  const router = useRouter();
 
   return (
     <section className="w-full md:w-[95%] my-10 sm:my-20 mx-auto flex flex-col items-center justify-center bg-transparent md:bg-white py-6 md:px-10 md:py-10 rounded-[20px] md:rounded-[30px]">
@@ -15,9 +13,9 @@ const ResetPassword: React.FC = () => {
             icon="ArrowLeft"
             iconPosition="left"
             textColor="#334EAC"
-            bgColor="transparent"
+            bgColor="#D0E3FF"
             hoverColor="#cdddf5"
-            className="border border-[#334EAC] !gap-0 md:!gap-2" 
+            className="!px-2 md:px-4 inline md:flex" 
             fontFamily="Poppins"
             fontWeight={500}
           >
