136e05d038289524dae6b5e0a69dad2ab6027138
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-05-15 12:32:39 -0500

    feat: add logic and examples for logic html

--- src/app/components/organisms/contact/index.tsx
+++ src/app/components/organisms/contact/index.tsx
@@ -4,27 +4,65 @@ import LabelAtom from "../../atoms/label";
 import TextAreaAtom from "../../atoms/textarea";
 import InputField from "../../molecules/inputField";
 
-const ContactSection: React.FC = () => {
-  return (
-    <section className="flex flex-col items-center py-10">
-      <div className="mb-8">
-        <LabelAtom
-          text="Contacto"
-          as="h1"
-          fontFamily="SFProDisplay"
-          fontWeight={700}
-          textColor="text-black"
-          className="text-5xl text-center"
-          fontSize="text-[64px]"
-        />
-      </div>
+type StyleMode = "default" | "kids";
+type ClassStyles = {
+  [key in StyleMode]: {
+    sectionStyle: string;
+    headStyle: string;
+    bodyStyle: string;
+    bodyContact: string;
+    bodyForm: string;
+  };
+};
 
-      <div className="flex items-center justify-center bg-[#5670CA] pb-4 w-full ">
-        <div className="hidden md:flex flex-col items-center justify-center w-1/3">
+interface ContactSectionProps {
+  style: StyleMode;
+}
+
+const ContactSection: React.FC<ContactSectionProps> = ({
+  style = "default",
+}) => {
+  const classStyles: ClassStyles = {
+    default: {
+      sectionStyle: "flex flex-col items-center py-10",
+      headStyle: "mb-8",
+      bodyStyle: "bg-[#5670CA] flex items-center justify-center pb-4 w-full",
+      bodyContact: "hidden md:flex flex-col items-center justify-center w-1/3",
+      bodyForm: "flex flex-col  w-full max-w-lg p-6 rounded-lg space-y-6",
+    },
+    kids: {
+      sectionStyle: "",
+      headStyle: "",
+      bodyStyle: "",
+      bodyContact: "",
+      bodyForm: "",
+    },
+  };
+  const { sectionStyle, headStyle, bodyStyle, bodyForm, bodyContact } =
+    classStyles[style];
+
+  return (
+    <section className={sectionStyle}>
+      {style !== "kids" && (
+        <div className={headStyle}>
+          <LabelAtom
+            text="Contacto"
+            as="h1"
+            fontFamily="SFProDisplay"
+            fontWeight={700}
+            textColor="text-black"
+            className="text-5xl text-center"
+            fontSize="text-[64px]"
+          />
+        </div>
+      )}
+      <div className={bodyStyle}>
+        {/*  */}
+        <div className={bodyContact}>
           <img src="/icon_contact.webp" alt="" />
         </div>
 
-        <form className="flex flex-col  w-full max-w-lg p-6 rounded-lg space-y-6">
+        <form className={bodyForm}>
           <InputField
             label="Correo:"
             name="correo"
@@ -36,6 +74,7 @@ const ContactSection: React.FC = () => {
             labelClassName="text-lg text-white "
             labelPosition="left"
           />
+          {/* Label Input Text Area */}
           <div>
             <LabelAtom
               fontFamily="Onest"
@@ -51,6 +90,7 @@ const ContactSection: React.FC = () => {
               height={100}
             />
           </div>
+          {/* Option Submit */}
           <Button
             type="submit"
             fontFamily="Poppins"
