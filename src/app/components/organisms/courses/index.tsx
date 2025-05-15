08d9349efcd70ac271107c2e3afa3c0c086b3cc0
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-05-15 17:25:06 -0500

    feat: Update CoursesOrganism

--- src/app/components/organisms/courses/index.tsx
+++ src/app/components/organisms/courses/index.tsx
@@ -6,8 +6,8 @@ import ButtonAtom from "../../atoms/button";
 function Courses() {
   return (
     <div className=" min-h-screen ">
-      <div className="grid grid-cols-3 gap-6 space-x-36 justify-between items-start max-w-7xl mx-auto">
-        <div className="col-span-2 ">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 space-x-36 justify-between items-start max-w-7xl mx-auto">
+        <div className="col-span-1 lg:col-span-2">
           <div className="grid grid-cols-2 gap-6 col-span-2">
             <div className="space-y-6">
               <CardCourses
@@ -18,6 +18,7 @@ function Courses() {
                 age="12 años"
                 price="200"
                 category="Programación"
+                withShadow={true}
               />
               <CardCourses
                 courseImage="/Course_minecraft.webp"
@@ -27,18 +28,11 @@ function Courses() {
                 age="12 años"
                 price="200"
                 category="Programación"
-              />
-              <CardCourses
-                courseImage="/Course_PythonKids-.webp"
-                alt="course1"
-                courseName="Python"
-                teacher="Juan Pérez"
-                age="12 años"
-                price="200"
-                category="Programación"
+                withShadow={true}
               />
             </div>
-            <div className="space-y-6 mt-12">
+
+            <div className="space-y-6 mt-12 hidden lg:block">
               <CardCourses
                 courseImage="/Course_Html.webp"
                 alt="course1"
@@ -47,6 +41,7 @@ function Courses() {
                 age="12 años"
                 price="200"
                 category="Programación"
+                withShadow={true}
               />
               <CardCourses
                 courseImage="/course_web.webp"
@@ -56,46 +51,47 @@ function Courses() {
                 age="12 años"
                 price="200"
                 category="Programación"
-              />
-              <CardCourses
-                courseImage="/Course_DesarrolloWeb.webp"
-                alt="course1"
-                courseName="Python"
-                teacher="Juan Pérez"
-                age="12 años"
-                price="200"
-                category="Programación"
+                withShadow={true}
               />
             </div>
           </div>
         </div>
 
-        <div className="flex items-center justify-center h-full">
-          <div className="w-full">
+        <div className="flex justify-center items-center h-full text-center">
+          <div className="flex flex-col items-center w-full">
             <LabelAtom
               as="h1"
               text="Abre las puertas al"
               textColor="text-black"
-              fontFamily="SFProDisplay"
+              fontFamily="BlackMango"
               fontWeight="bold"
               fontSize="text-[40px]"
               className="leading-tight"
             />
-            <div className="flex space-x-6">
+            <div className="flex flex-wrap lg:flex-nowrap gap-2 justify-center">
               <LabelAtom
                 as="h1"
                 text="conocimiento"
                 textColor="#334EAC"
-                fontFamily="SFProDisplay"
+                fontFamily="BlackMango"
                 fontWeight="bold"
                 fontSize="text-[40px]"
                 className="leading-tight"
               />
               <LabelAtom
                 as="h1"
-                text="con los"
+                text="con"
                 textColor="text-black"
-                fontFamily="SFProDisplay"
+                fontFamily="BlackMango"
+                fontWeight="bold"
+                fontSize="text-[40px]"
+                className="leading-tight"
+              />
+              <LabelAtom
+                as="h1"
+                text="los"
+                textColor="text-black"
+                fontFamily="BlackMango"
                 fontWeight="bold"
                 fontSize="text-[40px]"
                 className="leading-tight"
@@ -105,15 +101,15 @@ function Courses() {
               as="h1"
               text="cursos mejor certificados"
               textColor="text-black"
-              fontFamily="SFProDisplay"
+              fontFamily="BlackMango"
               fontWeight="bold"
               fontSize="text-[40px]"
               className="leading-tight whitespace-nowrap"
             />
             <ButtonAtom
-              bgColor="#334EAC"
-              hoverColor="#3B82F6"
-              textColor="#FFFFFF"
+              bgColor="#F9FCFF"
+              hoverColor="#E7E7E7"
+              textColor="#081F5C"
               fontFamily="Onest"
               fontWeight={500}
               icon="ArrowRight"
