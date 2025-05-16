1264a1b93124ce4240ef66ccbb8f04878c3f4ad7
franco13s <fnierihu@ucvvirtual.edu.pe>
2025-05-16 05:08:21 -0500

    avance de html

--- src/app/components/organisms/contact/index.tsx
+++ src/app/components/organisms/contact/index.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Button from "../../atoms/button/index";
+import DropdownAtom from "../../atoms/dropdown";
 import LabelAtom from "../../atoms/label";
 import TextAreaAtom from "../../atoms/textarea";
 import InputField from "../../molecules/inputField";
+import Image from "next/image";
 
 type StyleMode = "default" | "kids";
 type ClassStyles = {
@@ -41,9 +43,14 @@ const ContactSection: React.FC<ContactSectionProps> = ({
   const { sectionStyle, headStyle, bodyStyle, bodyForm, bodyContact } =
     classStyles[style];
 
+  const dropdownOptions = [
+    { id: "1", name: "Selecciona tu interés" },
+    { id: "2", name: "interés 1" },
+    { id: "3", name: "interés 2" },
+  ];
   return (
     <section className={sectionStyle}>
-      {style !== "kids" && (
+      {style === "kids" && (
         <div className={headStyle}>
           <LabelAtom
             text="Contacto"
@@ -56,52 +63,142 @@ const ContactSection: React.FC<ContactSectionProps> = ({
           />
         </div>
       )}
+      {style !== "kids" && (
+        <div className={headStyle}>
+          <LabelAtom
+            text="MANTENTE AL DÍA EN UGUIMAR"
+            as="h1"
+            fontFamily="SFProDisplay"
+            fontWeight={700}
+            textColor="text-black"
+            className="text-5xl text-right"
+            fontSize="text-[35px]"
+          />
+          <LabelAtom
+            text="Recibe las últimas novedades, lanzamientos de cursos y consejos"
+            as="h3"
+            fontFamily="SFProDisplay"
+            fontWeight={100}
+            textColor="text-black"
+            className="text-right"
+            fontSize="text-[25px]"
+          />
+          <LabelAtom
+            text="de aprendizaje directamente en tu correo"
+            as="h3"
+            fontFamily="SFProDisplay"
+            fontWeight={100}
+            textColor="text-black"
+            className="text-right"
+            fontSize="text-[25px]"
+          />
+        </div>
+      )}
+
       <div className={bodyStyle}>
-        {/*  */}
+        {style == 'kids' && (
         <div className={bodyContact}>
           <img src="/icon_contact.webp" alt="" />
         </div>
+        )}
 
-        <form className={bodyForm}>
-          <InputField
-            label="Correo:"
-            name="correo"
-            type="email"
-            placeholder="Email"
-            inputClassName="bg-[#F5F8FF] text-[#202020] rounded-md border border-gray-400 w-full rounded-xl"
-            labelFontFamily="Onest"
-            labelFontWeight="extrabold"
-            labelClassName="text-lg text-white "
-            labelPosition="left"
+        {style !== 'kids' && (
+        <div className={bodyContact}>
+          <Image src="/Logo.webp" alt="Logo" width={180} height={180} />
+          <LabelAtom
+            text="Inicio | Cursos | Planes | FAQ | Rutas | Feedback"
+            as="h3"
+            fontFamily="SFProDisplay"
+            fontWeight={500}
+            textColor="text-black"
+            className="text-left"
+            fontSize="text-[20px]"
           />
-          {/* Label Input Text Area */}
-          <div>
-            <LabelAtom
-              fontFamily="Onest"
-              fontWeight="extrabold"
-              text="Contacto"
-              className="mb-1"
+        </div>
+        )}
+        
+        {style !== "kids" && (
+          <form className={bodyForm}>
+            <div className="flex gap-4 mb-2">
+              <InputField
+                label=""
+                name="nombres"
+                type="text"
+                placeholder="Nombres"
+              />
+              <InputField
+                label=""
+                name="apellidos"
+                type="text"
+                placeholder="Apellidos"
+              />
+            </div>
+            <div className="mb-2">
+              <DropdownAtom
+                options={dropdownOptions}
+              />
+              <InputField
+                label=""
+                name="correo"
+                type="email"
+                placeholder="Correo electrónico"
+              />
+            </div>
+            <div>
+              <Button
+                type="submit"
+                bgColor="#334EAC"
+                hoverColor="#081F5C"
+                textColor="#fff"
+                iconPosition="right"
+                text="Suscribete ahora"
+              />
+            </div>
+          </form>
+        )}
+
+        {(style == 'kids' &&
+          <form className={bodyForm}>
+            <InputField
+              label="Correo:"
+              name="correo"
+              type="email"
+              placeholder="Email"
+              inputClassName="bg-[#F5F8FF] text-[#202020] rounded-md border border-gray-400 w-full rounded-xl"
+              labelFontFamily="Onest"
+              labelFontWeight="extrabold"
+              labelClassName="text-lg text-white "
+              labelPosition="left"
+            />
+            {/* Label Input Text Area */}
+            <div>
+              <LabelAtom
+                fontFamily="Onest"
+                fontWeight="extrabold"
+                text="Contacto"
+                className="mb-1"
+                textColor="#fff"
+              />
+              <TextAreaAtom
+                className="w-full rounded-xl"
+                name="mensaje"
+                placeholder="Escribe tu mensaje"
+                height={100}
+              />
+            </div>
+            {/* Option Submit */}
+            <Button
+              type="submit"
+              fontFamily="Poppins"
+              fontWeight={600}
+              bgColor="#334EAC"
+              hoverColor="#081F5C"
               textColor="#fff"
+              className="flex justify-center items-center w-full p-2 rounded-lg"
+              text="Enviar"
             />
-            <TextAreaAtom
-              className="w-full rounded-xl"
-              name="mensaje"
-              placeholder="Escribe tu mensaje"
-              height={100}
-            />
-          </div>
-          {/* Option Submit */}
-          <Button
-            type="submit"
-            fontFamily="Poppins"
-            fontWeight={600}
-            bgColor="#334EAC"
-            hoverColor="#081F5C"
-            textColor="#fff"
-            className="flex justify-center items-center w-full p-2 rounded-lg"
-            text="Enviar"
-          />
-        </form>
+          </form>
+        )}
       </div>
     </section>
   );
