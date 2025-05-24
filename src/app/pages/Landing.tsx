bf4da536683018b250c169cf87335187f3e0dd56
Jose Gastelo <joseluis_012004@hotmail.com>
2025-05-23 19:29:51 -0500

    feat: apply dynamic user mode in Landing based on selection (child, teen, adult)

--- src/app/pages/Landing.tsx
+++ src/app/pages/Landing.tsx
@@ -9,13 +9,31 @@ import FrequentQuestions from "../components/organisms/frequentQuestions";
 import Plans from "../components/organisms/plans";
 import ContactSection from "../components/organisms/contact";
 
-const Landing = () => {
+  const Landing = () => {
+  const [userMode, setUserMode] = useState("child");
+
+  useEffect(() => {
+    // Leer desde localStorage (por si ya se eligió antes)
+    const storedMode = localStorage.getItem("userMode");
+    if (storedMode) {
+      setUserMode(storedMode);
+    }
+
+    // Escuchar cambios
+    const handleUserModeChange = (e: any) => {
+      setUserMode(e.detail);
+    };
+
+    window.addEventListener("userModeChange", handleUserModeChange);
+    return () => window.removeEventListener("userModeChange", handleUserModeChange);
+  }, []);
+
 
   return (
     <>
       <div className="mt-12 space-y-36">
         <Hero />
-        <Courses mode="child" />
+        <Courses mode={userMode} />
         <CoursesExplorationSection />
         <LearningPathSection />
         <FeatureInstructor />
