18d72a9166e0d1fc9b2afc19774aeeedc1e66692
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2025-05-25 19:23:04 -0500

    fix: correction, use courses-exploration logic

--- src/app/components/organisms/contact/index.tsx
+++ src/app/components/organisms/contact/index.tsx
@@ -8,7 +8,8 @@ import Image from "next/image";
 import { useUserType } from "../../organisms/header";
 
 function Contact() {
-  const { userType } = useUserType(); 
+  const { userType } = useUserType();
+  const isKid = userType === "child";
   const activeMode = userType === "child" ? "child" : "adult"; // ✅ ahora usa "child", no "kids"
 
   const dropdownOptions = [
@@ -17,41 +18,20 @@ function Contact() {
     { id: "3", name: "interés 2" },
   ];
 
-  const className = {
-    adult: {
-      background: "absolute z-[-1] bottom-0 left-0 w-full h-full [&>img]:hidden",
-      content: "flex flex-col-reverse md:flex-row items-start md:justify-between w-full gap-6",
-      left: "flex flex-col items-start w-full justify-center py-5 md:w-1/3",
-      right: "flex flex-col items-center md:items-end w-full md:w-auto",
-      background_inputs: "#334EAC",
-      dropdown: "text-[#FFFFFF] rounded-xl",
-      inputClassName: "text-white w-full rounded-xl px-4 py-3 placeholder:text-white",
-    },
-    child: {
-      background: "absolute z-[-1] bottom-0 left-0 w-full h-full bg-[#E1F1F6]",
-      content: "flex flex-col-reverse md:flex-row items-start md:justify-between w-full gap-6",
-      left: "flex flex-col items-start w-full justify-center py-5 md:w-1/3",
-      right: "flex flex-col items-center md:items-end w-full md:w-auto",
-      background_inputs: "#FFFFFF",
-      dropdown: "text-[#000000] rounded-xl",
-      inputClassName: "text-[#202020] w-full rounded-xl px-4 py-3",
-    },
-  };
-
-  const {
-    background,
-    content,
-    left,
-    right,
-    background_inputs,
-    dropdown,
-    inputClassName,
-  } = className[activeMode];
+  // classStyles
+  const classNameBackground = isKid ? "bg-[#E1F1F6]" : "[&>img]:hidden";
+  const background_inputs = isKid ? "#FFFFFF" : "#334EAC";
+  const classNameDropdown = isKid ? "text-[#000000] " : "text-[#FFFFFF]";
+  const classNameInput = isKid
+    ? "text-[#202020]"
+    : "text-white placeholder:text-white";
 
   return (
     <div className="relative flex flex-col items-center py-22 px-6 md:px-30">
       {/* background */}
-      <div className={background}>
+      <div
+        className={`absolute z-[-1] bottom-0 left-0 w-full h-full ${classNameBackground}`}
+      >
         <Image
           src="/sea_background.webp"
           alt="Fondo Kids"
@@ -62,11 +42,16 @@ function Contact() {
       </div>
 
       {/* Content */}
-      <div className={content}>
+      <div className="flex flex-col-reverse md:flex-row items-start md:justify-between w-full gap-6">
         {/* left */}
-        <div className={left}>
+        <div className="flex flex-col items-start w-full justify-center py-5 md:w-1/3">
           <div className="relative w-[100px] h-[50px] md:w-[150px] md:h-[60px]">
-            <Image src="/Logo.webp" alt="Logo" fill className="object-contain" />
+            <Image
+              src="/Logo.webp"
+              alt="Logo"
+              fill
+              className="object-contain"
+            />
           </div>
           <LabelAtom
             text="Inicio  |  Cursos  |  Planes  |  FAQ  |  Rutas  |  Feedback"
@@ -97,7 +82,7 @@ function Contact() {
         </div>
 
         {/* right */}
-        <div className={right}>
+        <div className="flex flex-col items-center md:items-end w-full md:w-auto">
           <div className="mt-3 w-full flex flex-col items-center md:items-end text-center md:text-right">
             <LabelAtom
               className="flex-col py-3 items-end leading-tight"
@@ -136,7 +121,7 @@ function Contact() {
                 type="text"
                 placeholder="Nombres"
                 colorBG={background_inputs}
-                inputClassName={inputClassName}
+                inputClassName={`w-full rounded-xl px-4 py-3 ${classNameInput}`}
               />
               <InputField
                 label=""
@@ -144,7 +129,7 @@ function Contact() {
                 type="text"
                 placeholder="Apellidos"
                 colorBG={background_inputs}
-                inputClassName={inputClassName}
+                inputClassName={`w-full rounded-xl px-4 py-3 ${classNameInput}`}
               />
             </div>
 
@@ -158,7 +143,7 @@ function Contact() {
                 borderWeight={0}
                 textSize="base"
                 fullWidth={true} // ✅ asegura que use todo el ancho
-                className={dropdown}
+                className={`rounded-xl ${classNameDropdown}`}
               />
               <InputField
                 className="mt-2"
@@ -167,7 +152,7 @@ function Contact() {
                 type="email"
                 placeholder="Correo electrónico"
                 colorBG={background_inputs}
-                inputClassName={inputClassName}
+                inputClassName={`w-full rounded-xl px-4 py-3 ${classNameInput}`}
               />
             </div>
 
